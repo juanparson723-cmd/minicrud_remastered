0776c26a4b6c50fd8aa115c4ff5f2adf3a42e273
FER <juanparson723@gmail.com>
2025-10-25 23:06:26 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
+from models import *
+from routes import *
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'mi_clave_secreta'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///minicrud.db'
+
+uri = os.getenv("DATABASE_URL", "sqlite:///local.db")
+
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 # Importar las rutas y los modelos
-from routes import *
-from models import *
 
 if __name__ == '__main__':
     app.run(debug=True)
